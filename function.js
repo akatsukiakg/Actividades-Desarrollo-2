ff578cc767c4545aeaf5ce34c4faa8a4c450125a
Walter Carrasco <95136445+someone1a@users.noreply.github.com>
2023-04-25 15:33:33 -0300

    Correcciones

--- function.js
+++ function.js
@@ -1,10 +1,10 @@
 function validateDate(day, month, year) {
-    if (month === 2 && day <=28 || month === 4 && day <= 31 || month === 6 && day <=31 || month === 1 && day <=30 || month === 3 && day <=30 || month === 5 && day <=30 || month === 7 && day <=30 || month === 8 && day <=30 || month === 9 && day <=30 || month === 10 && day <=30 || month === 11 && day <=30 || month === 12 && day <=30) {
+    if (month === 2 && day <=28 || month === 4 && day <= 31 || month === 6 && day <=31 || month === 1 && day <=31 || month === 3 && day <=31 || month === 5 && day <=31 || month === 7 && day <=31 || month === 8 && day <=31 || month === 9 && day <=30 || month === 10 && day <=31 || month === 11 && day <=30 || month === 12 && day <=31) {
         return true;
     }else {
         return false
     }
-    if (year === 2 % 4 && year % 4 === 0) {
+    if (year % 4 === 0 && day <=29) {
         return true;
     } else {
         return false;
